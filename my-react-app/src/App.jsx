607db3b45ced6d5302a38f2fbb93ed18c99dab26
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-01-15 00:28:12 +0530

    Fetchin Quotes Functionality

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Counter from "./counter";
+import QuoteFetcher from "./QuoteFetcher";
 function App() {
   return (
     <>
-      <Counter />
+      {/* <Counter /> */}
+      <QuoteFetcher />
     </>
   );
 }
