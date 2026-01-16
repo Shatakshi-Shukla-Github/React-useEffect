159086b1bb12dbc31d1a498e6d3513e2b31b56a1
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-01-17 01:34:45 +0530

    Adding a Loader

--- my-react-app/src/QuoteFetcher.jsx
+++ my-react-app/src/QuoteFetcher.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
 export default function QuoteFetcher() {
   const [quote, setQuote] = useState({ text: "", author: "" });
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetchQuote();
   }, []);
@@ -11,12 +12,14 @@ export default function QuoteFetcher() {
     const jsonResponse = await response.json();
     const randomQuote = jsonResponse.quote;
     setQuote(randomQuote);
+    setIsLoading(false);
   }
   return (
     <div>
-      <button onClick={fetchQuote}>Get Quote</button>
+      {isLoading && <div class="loader"></div>}
       <h1>{quote.text}</h1>
       <h3>{quote.author}</h3>
+      {!isLoading && <button onClick={fetchQuote}>Get Quote</button>}
     </div>
   );
 }
