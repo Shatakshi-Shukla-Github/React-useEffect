f9de944c0c4d2fa4f6aceaf29e1d218cbf384d34
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-01-18 22:06:01 +0530

    Github Search Functionality

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Counter from "./counter";
 import QuoteFetcher from "./QuoteFetcher";
+import ProfileSearchForm from "./ProfileSearchForm";
+import ProfileViewerWithSearch from "./ProfileViewerWithSearch";
 function App() {
   return (
     <>
       {/* <Counter /> */}
-      <QuoteFetcher />
+      {/* <QuoteFetcher /> */}
+      <ProfileViewerWithSearch />
     </>
   );
 }
